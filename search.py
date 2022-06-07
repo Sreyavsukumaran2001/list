339b1d090e884f33b0ff566b7a8969c8bbf45e2e
nandukunjumon <nandukunjumon16@gmail.com>
2022-06-07 11:31:33 +0530

    search

---
+++ search.py
@@ -0,0 +1,8 @@
+lst1=[10,11,12,13,14]
+lst2=[11,14,15,16,17]
+dup_lst=[]
+
+for num in lst1:
+    if num in lst2:
+        dup_lst.append(num)
+print(dup_lst)
---
+++ searching.py
@@ -0,0 +1,39 @@
+# lst=[10,11,12,14,15,16,17]
+# element=11
+#
+# flag=0
+# for num in lst:
+#     if element==num:
+#         flag=1
+#         break
+# print ("element found" if flag!=0 else "not found")
+#
+# for i in range(0,len(lst)):
+#     if element==lst[i]:
+#         flag=1
+#         break
+# print("element found" if flag!=0 else "not found")
+
+# lst=[10,11,12,13,14,15,16,17,12,12,12]
+# evenlist=[]
+# for num in lst:
+#     if num%2==0:
+#         evenlist.append(num)
+# print(evenlist)
+# evenlist.sort(reverse=True)
+# print(evenlist)
+# #
+# lst.insert(3,50)
+# print(lst)
+# print(lst.count(12))
+
+# n=17
+# for num in lst:
+#     if n==num:
+#         print("found")
+#         break
+#     else:
+#         print("not found")
+#         break
+
+
